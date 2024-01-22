fc199bd1874871360705396b365d4d86303f92b6
CheungKamYau <126758580+CheungKamYau@users.noreply.github.com>
2024-01-22 14:01:35 +0800

    Finish setup

---
+++ chicker/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import logo from "./chicker.jpg";
+
+function App() {
+  return (
+    <div className="App">
+      <img src={logo} />
+      <p>CSCI3100 project</p>
+    </div>
+  );
+}
+
+export default App;

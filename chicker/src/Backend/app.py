7c76af3bccca8f3cc84378399809f073e5600306
VTiS15 <vtis.tsangvincent15@gmail.com>
2024-04-06 22:21:13 +0800

    Remove Mongo URI

---
+++ chicker/src/Backend/app.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask_restful import Api
+from login import login_manager
+from resources.user import *
+
+app = Flask(__name__)
+app.config[
+    "MONGO_URI"
+] = MONGO_URI  # Assign connection string before running!
+app.secret_key = None  # Assign random string before running!
+
+login_manager.init_app(app)
+api = Api(app)
+
+api.add_resource(UserLogin, "/login")
+api.add_resource(UserLogout, "/logout")
+api.add_resource(UserRegister, "/register")
+api.add_resource(UserDelete, "/user/delete")
+api.add_resource(UserStatusChange, "/status")
+api.add_resource(UserFollow, "/follow")
+api.add_resource(UserUnfollow, "/unfollow")
+api.add_resource(UserRecommend, "/user/recommend")
+api.add_resource(UserUpdate, "/user/update")
+
+if __name__ == "__main__":
+    app.run(debug=True)

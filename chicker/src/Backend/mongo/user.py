3198200dff65b7d47d197818e87fea2f4fddf5a4
VTiS15 <vtis.tsangvincent15@gmail.com>
2024-04-06 22:11:48 +0800

    Change collection folder name to mongo

---
+++ chicker/src/Backend/mongo/user.py
@@ -0,0 +1,63 @@
+import random
+import string
+from datetime import datetime
+
+import gridfs
+from db import user_db
+
+
+class User:
+    def __init__(
+        self,
+        username,
+        email,
+        password_hash,
+        user_id=f"u{''.join(random.choices(string.ascii_letters + string.digits, k=12))}",
+        icon_id=None,
+        followers=[],
+        followees=[],
+        bio="",
+        is_admin=False,
+        date=datetime.now(),
+    ):
+        self.username = username
+        self.user_id = user_id
+        self.password_hash = password_hash
+        self.email = email
+        self.icon_id = icon_id
+        self.followers = followers
+        self.followees = followees
+        self.bio = bio
+        self.is_admin = is_admin
+        self.date = date
+
+    @staticmethod
+    def is_authenticated():
+        return True
+
+    @staticmethod
+    def is_active():
+        return True
+
+    @staticmethod
+    def is_anonymous():
+        return False
+
+    def get_id(self):
+        return self.user_id
+
+    def save(self):
+        return user_db.user.insert_one(
+            {
+                "username": self.username,
+                "user_id": self.user_id,
+                "password_hash": self.password_hash,
+                "email": self.email,
+                "icon_id": self.icon_id,
+                "followers": self.followers,
+                "followees": self.followees,
+                "bio": self.bio,
+                "is_admin": self.is_admin,
+                "date": self.date,
+            }
+        )

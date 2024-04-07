09e3fd646c60cb8fff31168f3c2545eeb1b1d53c
JackIp2025 <ipmingc@gmail.com>
2024-04-07 23:01:18 +0800

    vt help me add delete comment plz

--- chicker/src/Backend/db.py
+++ chicker/src/Backend/db.py
@@ -1,6 +1,6 @@
 from flask_pymongo import pymongo
 
-connection_string = "mongodb+srv://csci3100:chicker@cluster0.7tf1swn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" # Assign connection string before running!
+connection_string = None # Assign connection string before running!
 
 client = pymongo.MongoClient(connection_string)
 

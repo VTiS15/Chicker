86a62f5102d126f13b3c7c59c90a837fdf51cc7e
VTiS15 <vtis.tsangvincent15@gmail.com>
2024-04-07 11:35:25 +0800

    Initial commit

--- chicker/src/Backend/app.py
+++ chicker/src/Backend/app.py
@@ -3,10 +3,11 @@ from flask import Flask
 from flask_restful import Api
 from login import login_manager
 from resources.user import *
+from resources.chat import *
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = connection_string
-app.secret_key = "diu"  # Assign random string before running!
+app.secret_key = None  # Assign random string before running!
 
 login_manager.init_app(app)
 api = Api(app)
@@ -20,6 +21,8 @@ api.add_resource(UserFollow, "/follow")
 api.add_resource(UserUnfollow, "/unfollow")
 api.add_resource(UserRecommend, "/user/recommend")
 api.add_resource(UserUpdate, "/user/update")
+
+api.add_resource(ChatCreate, "/chat/create")
 
 if __name__ == "__main__":
     app.run(debug=True)

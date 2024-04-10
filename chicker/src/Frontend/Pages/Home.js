cedb368b27084c88cfb25c0b3d49e608a3efebda
CheungKamYau <126758580+CheungKamYau@users.noreply.github.com>
2024-04-10 16:48:06 +0800

    WIP: post function

--- chicker/src/Frontend/Pages/Home.js
+++ chicker/src/Frontend/Pages/Home.js
@@ -1,3 +1,5 @@
+import { useState, useRef } from "react";
+
 import "./Home.css";
 import Sidebar from "../components/sidebar.js";
 import PostList from "../components/posts.js";
@@ -5,167 +7,69 @@ import image from "../Pictures/image.png";
 import video from "../Pictures/video.png";
 import { getStyling } from "../functions/style.js";
 import { getUserLogin } from "./LoginPage.js";
+import { post } from "../functions/dummyPosts.js";
 
-import dummy1 from "../Pictures/dummyPictures/dummy (1).png";
-import dummy2 from "../Pictures/dummyPictures/dummy (2).png";
-import dummy3 from "../Pictures/dummyPictures/dummy (3).png";
-import dummy4 from "../Pictures/dummyPictures/dummy (4).png";
-import dummy5 from "../Pictures/dummyPictures/dummy (5).png";
-import dummy6 from "../Pictures/dummyPictures/dummy (6).png";
-import video1 from "../Pictures/dummyPictures/video.mp4";
+import profilepic from "../Pictures/UserNeedLogin.jpeg";
 
-import profile1 from "../Pictures/dummyPictures/profile (1).jpg";
-import profile2 from "../Pictures/dummyPictures/profile (2).jpg";
-import profile3 from "../Pictures/dummyPictures/profile (3).jpg";
-import profile4 from "../Pictures/dummyPictures/profile (4).jpg";
-import profile5 from "../Pictures/dummyPictures/profile (5).jpg";
-
-const posts = [
-  {
-    postID: 1,
-    user: {
-      username: "JohnDoe",
-      profilePicture: profile1,
-    },
-    timestamp: "2 hours ago",
-    text: "This is the first post. Lorem ipsum dolor sit amet, adipiscing elit. In this example, the component maintains a state variable isTextColored using the useState hook. It starts with an initial value of false. The handleButtonClick function is responsible for toggling the value of isTextColored when the button is clicked. It uses the setIsTextColored function to update the state. The button's className is conditionally set based on the value of isTextColored. If isTextColored is true, the button will have the additional class colored. You can define CSS styles for the colored class to change the text color. When the button is clicked, the handleButtonClick function is invoked, triggering the state update and causing a re-render of the component. As a result, the button's text color will toggle based on the value of isTextColored. This is the first post. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In this example, the ButtonTextColorToggle component maintains a state variable isTextColored using the useState hook. It starts with an initial value of false. The handleButtonClick function is responsible for toggling the value of isTextColored when the button is clicked. It uses the setIsTextColored function to update the state. The button's className is conditionally set based on the value of isTextColored. If isTextColored is true, the button will have the additional class colored. You can define CSS styles for the colored class to change the text color. When the button is clicked, the handleButtonClick function is invoked, triggering the state update and causing a re-render of the component. As a result, the button's text color will toggle based on the value of isTextColored. This is the first post. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In this example, the ButtonTextColorToggle component maintains a state variable isTextColored using the useState hook. It starts with an initial value of false. The handleButtonClick function is responsible for toggling the value of isTextColored when the button is clicked. It uses the setIsTextColored function to update the state. The button's className is conditionally set based on the value of isTextColored. If isTextColored is true, the button will have the additional class colored. You can define CSS styles for the colored class to change the text color. When the button is clicked, the handleButtonClick function is invoked, triggering the state update and causing a re-render of the component. As a result, the button's text color will toggle based on the value of isTextColored. This is the first post. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In this example, the ButtonTextColorToggle component maintains a state variable isTextColored using the useState hook. It starts with an initial value of false. The handleButtonClick function is responsible for toggling the value of isTextColored when the button is clicked. It uses the setIsTextColored function to update the state. The button's className is conditionally set based on the value of isTextColored. If isTextColored is true, the button will have the additional class colored. You can define CSS styles for the colored class to change the text color. When the button is clicked, the handleButtonClick function is invoked, triggering the state update and causing a re-render of the component. As a result, the button's text color will toggle based on the value of isTextColored.",
-    image: dummy1,
-    video: null,
-    likes: 10,
-    comments: 5,
-    commentContent: [
-      { image: profile1, commenter: "John", comment: "On 1st post" },
-      {
-        image: profile2,
-        commenter: "Jack",
-        comment:
-          "1st post. post. dolor sit amet, adipiscing elit. In this example, the component maintains a state variable. Lorem ipsum dolor sit amet, adipiscing elit. In this example, the component maintains a state variable.",
-      },
-      { image: profile3, commenter: "Jason", comment: "1st post, fantastic" },
-      { image: profile3, commenter: "Jason", comment: "1st post, fantastic" },
-      { image: profile3, commenter: "Jason", comment: "1st post, fantastic" },
-      { image: profile3, commenter: "Jason", comment: "1st post, fantastic" },
-      { image: profile3, commenter: "Jason", comment: "1st post, fantastic" },
-      { image: profile3, commenter: "Jason", comment: "1st post, fantastic" },
-      { image: profile3, commenter: "Jason", comment: "1st post, fantastic" },
-      { image: profile3, commenter: "Jason", comment: "1st post, fantastic" },
-    ],
-    shares: 3,
-  },
-  {
-    postID: 2,
-    user: {
-      username: "JaneSmith",
-      profilePicture: profile2,
-    },
-    timestamp: "1 day ago",
-    text: "Check out this beautiful view!",
-    image: null,
-    video: video1,
-    likes: 7,
-    comments: 2,
-    commentContent: [
-      {
-        image: profile1,
-        commenter: "John",
-        comment:
-          "2nd post, Lorem ipsum dolor sit amet, adipiscing elit. In this example, the component maintains a state variable. Lorem ipsum dolor sit amet, adipiscing elit. In this example, the component maintains a state variable.",
-      },
-      { image: profile2, commenter: "Jack", comment: "2nd post, Nice" },
-    ],
-    shares: 1,
-  },
-  {
-    postID: 3,
-    user: {
-      username: "MikeJohnson",
-      profilePicture: profile3,
-    },
-    timestamp: "3 days ago",
-    text: "Just had an amazing meal at the new restaurant!",
-    image: null,
-    video: null,
-    likes: 15,
-    comments: 8,
-    commentContent: [
-      {
-        image: profile1,
-        commenter: "John",
-        comment:
-          "3rd post, Lorem ipsum dolor sit amet, adipiscing elit. In this example, the component maintains a state variable. Lorem ipsum dolor sit amet, adipiscing elit. In this example, the component maintains a state variable.",
-      },
-      { image: profile2, commenter: "Jack", comment: "3d post, Nice" },
-      { image: profile3, commenter: "John", comment: "3rd post, Nice" },
-      { image: profile4, commenter: "Jack", comment: "3rd post, Nice" },
-      { image: profile2, commenter: "Jack", comment: "3d post, Nice" },
-      { image: profile3, commenter: "John", comment: "3rd post, Nice" },
-      { image: profile4, commenter: "Jack", comment: "3rd post, Nice" },
-      { image: profile2, commenter: "Jack", comment: "3d post, Nice" },
-      { image: profile3, commenter: "John", comment: "3rd post, Nice" },
-      { image: profile4, commenter: "Jack", comment: "3rd post, Nice" },
-      { image: profile2, commenter: "Jack", comment: "3d post, Nice" },
-      { image: profile3, commenter: "John", comment: "3rd post, Nice" },
-      { image: profile4, commenter: "Jack", comment: "3rd post, Nice" },
-      { image: profile2, commenter: "Jack", comment: "3d post, Nice" },
-      { image: profile3, commenter: "John", comment: "3rd post, Nice" },
-      { image: profile4, commenter: "Jack", comment: "3rd post, Nice" },
-      { image: profile2, commenter: "Jack", comment: "3d post, Nice" },
-      { image: profile3, commenter: "John", comment: "3rd post, Nice" },
-      { image: profile4, commenter: "Jack", comment: "3rd post, Nice" },
-      { image: profile2, commenter: "Jack", comment: "3d post, Nice" },
-      { image: profile3, commenter: "John", comment: "3rd post, Nice" },
-      { image: profile4, commenter: "Jack", comment: "3rd post, Nice" },
-      { image: profile2, commenter: "Jack", comment: "3d post, Nice" },
-      { image: profile3, commenter: "John", comment: "3rd post, Nice" },
-      { image: profile4, commenter: "Jack", comment: "3rd post, Nice" },
-      { image: profile2, commenter: "Jack", comment: "3d post, Nice" },
-      { image: profile3, commenter: "John", comment: "3rd post, Nice" },
-      { image: profile4, commenter: "Jack", comment: "3rd post, Nice" },
-      { image: profile2, commenter: "Jack", comment: "3d post, Nice" },
-      { image: profile3, commenter: "John", comment: "3rd post, Nice" },
-      { image: profile4, commenter: "Jack", comment: "3rd post, Nice" },
-    ],
-    shares: 2,
-  },
-  {
-    postID: 4,
-    user: {
-      username: "Johnny",
-      profilePicture: profile4,
-    },
-    timestamp: "4 day ago",
-    text: "Check out this beautiful view!",
-    image: dummy4,
-    video: null,
-    likes: 7,
-    comments: 2,
-    commentContent: [],
-    shares: 1,
-  },
-  {
-    postID: 5,
-    user: {
-      username: "JamesMcGill",
-      profilePicture: profile5,
-    },
-    timestamp: "8 day ago",
-    text: "Check out this beautiful view!",
-    image: dummy5,
-    video: null,
-    likes: 7,
-    comments: 2,
-    commentContent: [
-      { image: profile1, commenter: "Jack", comment: "5th post, Nice" },
-    ],
-    shares: 1,
-  },
-];
-
-const commentsOfEachPost = posts.map((post) => post.comments);
+const posts = post;
 const styling = getStyling();
+const commentsOfEachPost = posts.map((post) => post.comments);
 
 export default function Home() {
   const isLoggedIn = getUserLogin();
+  const [postText, setPostText] = useState("");
+  const [uploadedImage, setUploadedImage] = useState(null);
+  const [uploadedVideo, setUploadedVideo] = useState(null);
+  const [fileType, setFileType] = useState("");
+  const date = new Date();
+  const photoInputRef = useRef();
+
+  const handleClickImage = () => {
+    photoInputRef.current.click();
+    setFileType("image/png, image/jpg, image/jpeg");
+  };
+
+  const handleClickVideo = () => {
+    photoInputRef.current.click();
+    setFileType("video/mp4");
+  };
+
+  const handleUplaod = (e) => {
+    const selectedFile = e.target.files[0];
+    if (selectedFile) {
+      setUploadedImage(URL.createObjectURL(selectedFile));
+    }
+  };
+
+  const handleClear = () => {
+    setPostText("");
+    setUploadedImage(null);
+    setUploadedVideo(null);
+  };
+
+  const handleSubmit = (Text, image, video) => {
+    if (Text || image || video) {
+      posts.unshift({
+        postID: posts.length + 1,
+        user: {
+          username: "JC",
+          profilePicture: profilepic,
+        },
+        timestamp: date.toLocaleTimeString(),
+        text: Text,
+        image: image,
+        video: video,
+        likes: 0,
+        comments: 0,
+        commentContent: [],
+        shares: 0,
+      });
+      handleClear();
+    } else {
+      alert("Say something before sending it out");
+    }
+  };
+
   return (
     <div className="Home" style={{ styling }}>
       <Sidebar />
@@ -177,15 +81,54 @@ export default function Home() {
               className="newPostText"
               cols="1"
               placeholder="write something..."
-            ></textarea>
+              value={postText}
+              onChange={(e) => setPostText(e.target.value)}
+            />
+            {(uploadedImage || uploadedVideo) && (
+              <img
+                style={{ maxWidth: "20%", borderRadius: "12px" }}
+                src={uploadedImage}
+              />
+            )}
             <div class="button-container">
-              <button class="left-button">
-                <img src={image}></img>
+              <input
+                type="file"
+                accept={fileType}
+                ref={photoInputRef}
+                onChange={handleUplaod}
+                style={{ display: "none" }}
+              />
+              <button class="left-button" onClick={handleClickImage}>
+                <img src={image} />
               </button>
-              <button class="left-button">
-                <img src={video}></img>
+
+              <button class="left-button" onClick={handleClickVideo}>
+                <img src={video} />
               </button>
-              <button class="right-button">Post</button>
+
+              {(postText || uploadedImage || uploadedVideo) && (
+                <button
+                  onClick={() => handleClear()}
+                  style={{
+                    background: "transparent",
+                    cursor: "pointer",
+                    padding: "4px",
+                    margin: "4px",
+                    borderRadius: "12px",
+                  }}
+                  className="left-button"
+                >
+                  clear
+                </button>
+              )}
+              <button
+                class="right-button"
+                onClick={() =>
+                  handleSubmit(postText, uploadedImage, uploadedVideo)
+                }
+              >
+                Post
+              </button>
             </div>
           </div>
         ) : (
